aec2b59397946f7650c2d83454ab5075760ce9d0
Vishwash kamal <63347077+vishwashk14@users.noreply.github.com>
2024-02-29 10:23:09 -0800

    animation-letter

---
+++ animated lettter/script.js
@@ -0,0 +1,9 @@
+const container = document.querySelectorAll('span')
+
+
+
+container.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active')
+    })
+})
